bdd324c501ffe64bb4ab8ae91ec0002e98b9b752
azkadevanda <azkadevanda8@gmail.com>
2026-06-27 00:45:34 +0700

    fix issue svc user management

--- ocs-svc-role/src/main/java/com/ocs/portal/svc/role/controller/roles/PortalController.java
+++ ocs-svc-role/src/main/java/com/ocs/portal/svc/role/controller/roles/PortalController.java
@@ -1,13 +1,13 @@
 package com.ocs.portal.svc.role.controller.portal;
 
 import com.ocs.portal.dto.response.BaseResponseDto;
-import com.ocs.portal.svc.role.service.PortalService;
+import com.ocs.portal.svc.role.service.roles.PortalService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Tag(name = "Prod Portal Controller", description = "Portal Management APIs")
 @RestController
@@ -19,7 +19,7 @@ public class PortalController {
     @Autowired
     PortalService portalService;
 
-   @GetMapping("{portalId}/dirMenus")
+  @GetMapping("{portalId}/dirMenus")
   public ResponseEntity<BaseResponseDto> qryListDirMenus(@PathVariable Long portalId, @RequestParam Long spId) {
     return portalService.qryListDirMenusPortal(portalId, spId);
   }
