51bdd18f97cce3ede5604a91b0af30eaf32d5d32
azkadevanda <azkadevanda8@gmail.com>
2026-06-27 00:36:24 +0700

    fix issue portal controller

--- ocs-svc-role/src/main/java/com/ocs/portal/svc/role/controller/portal/PortalController.java
+++ ocs-svc-role/src/main/java/com/ocs/portal/svc/role/controller/portal/PortalController.java
@@ -1,19 +1,13 @@
-package com.ocs.portal.svc.role.controller;
+package com.ocs.portal.svc.role.controller.portal;
 
-import com.ocs.portal.svc.role.dto.request.PortalFilterDto;
-import com.ocs.portal.svc.role.dto.request.common.PagingRequestDto;
-import com.ocs.portal.svc.role.dto.request.roles.PortalRoleDto;
+import com.ocs.portal.dto.response.BaseResponseDto;
 import com.ocs.portal.svc.role.service.PortalService;
-import com.ocs.portal.dto.response.CustomeResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "Prod Portal Controller", description = "Portal Management APIs")
 @RestController
